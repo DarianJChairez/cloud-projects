c812f6e31173f42b7f273e6159109ae6ead9ad1f
Darian J. Chairez <jaycechairez@gmail.com>
2025-09-17 20:13:10 -0400

    Update provider.tf

--- terraform-cloud-projects/azure/resource-group-lab/provider.tf
+++ terraform-cloud-projects/azure/resource-group-lab/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.44.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+
+  subscription_id = "c2670834-80cd-4e37-8535-a6f50e51a4c2"
+}
